d0863f18cd06008a172e043f98694c3982d7d139
SANTIAGO GACÍA GRANDA <santiago.garcia09@uceva.edu.co>
2026-04-02 00:57:12 -0500

    Initial commit - Spring Boot project setup

---
+++ src/test/java/com/santiago/inventory_system/InventorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santiago.inventory_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
